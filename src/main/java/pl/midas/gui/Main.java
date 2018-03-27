3b1ea3b48202e4bb071435be4f8742e9cf90adc5
Sergiusz <deephans@o2.pl>
2018-03-27 18:23:01 +0200

    added JavaFX support

---
+++ src/main/java/pl/midas/gui/Main.java
@@ -0,0 +1,20 @@
+package pl.midas.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
